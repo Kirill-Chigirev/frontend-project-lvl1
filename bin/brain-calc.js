0e6bc2f977500b1111a826ce64f7c87f5a109bf1
Kirill-Chigirev <chigirevk@bk.ru>
2022-09-14 14:53:48 +0700

    add calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,30 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-
-const calc = () => {
-  const operations = ['+', '-', '*'];
-  const getRandomArrayElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
-  const getRandomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-  const randExp = () => `${getRandomInRange(0, 29)} ${getRandomArrayElement(operations)} ${getRandomInRange(0, 29)}`;
-
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('What is the result of the expression?');
-  for (let i = 0; i < 3; i += 1) {
-    const expression = randExp();
-    console.log(`Question: ${expression}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (expression === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${expression}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
+import { calc } from '../src/index.js';
 
 calc();
---
+++ src/index.js
@@ -0,0 +1,43 @@
+import readlineSync from 'readline-sync';
+import {
+  cons, car, cdr, toString, isPair,
+} from '@hexlet/pairs';
+// eslint-disable-next-line import/prefer-default-export
+export const calc = () => {
+  const operations = ['+', '-', '*'];
+  const getRandomArrayElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
+  const getRandomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('What is the result of the expression?');
+  for (let i = 0; i < 3; i += 1) {
+    const randomOperator = getRandomArrayElement(operations);
+    const pair = cons(getRandomInRange(0, 29), getRandomInRange(0, 29));
+    const randExp = `${car(pair)} ${randomOperator} ${cdr(pair)}`;
+    console.log(`Question: ${randExp}`);
+    const answer = readlineSync.question('Your answer: ');
+    const getRightAnswer = (a, b, op) => {
+      let result;
+      if (op === '+') {
+        result = a + b;
+      } else if (op === '*') {
+        result = a * b;
+      } else {
+        result = a - b;
+      }
+
+      return result;
+    };
+    const rightAnswer = getRightAnswer(car(pair), cdr(pair), randomOperator);
+    if (rightAnswer === Number(answer)) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
